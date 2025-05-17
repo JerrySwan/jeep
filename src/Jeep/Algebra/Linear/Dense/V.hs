2a1352817968d47c80845ae86fee62e619428a01
John Berthels <john.berthels@gmail.com>
2025-05-17 09:22:32 +0100

    add sub (over ring)

--- src/Jeep/Algebra/Linear/Dense/V.hs
+++ src/Jeep/Algebra/Linear/Dense/V.hs
@@ -10,16 +10,19 @@ type V s = [s]
 
 -----------------
 
-vscale :: SR.Semiring s => s -> V s -> V s
+vscale :: (SR.Semiring s) => s -> V s -> V s
 vscale c = map (SR.times c)
 
-vadd :: SR.Semiring s => V s -> V s -> V s
+vadd :: (SR.Semiring s) => V s -> V s -> V s
 vadd xs = zipWith (SR.+) xs
 
-vmul :: SR.Semiring s => V s -> V s -> s
+vsub :: (SR.Ring s) => V s -> V s -> V s
+vsub xs = zipWith (SR.-) xs
+
+vmul :: (SR.Semiring s) => V s -> V s -> s
 vmul xs = foldr SR.plus SR.zero . zipWith (SR.*) xs
 
 vnorm2 :: (SR.Semiring s, Floating s) => V s -> s
-vnorm2 v = sqrt (vmul v v) 
+vnorm2 v = sqrt (vmul v v)
 
 -- End ---------------------------------------------------------------
