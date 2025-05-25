b2a5db17532258e1dc94016ea5bab4dbd1fe9c9c
Jerry Swan <dr.jerry.swan@gmail.com>
2025-05-25 09:31:39 +0100

    Add treeToGraph, forestToGraph

--- src/Jeep/Data/Graph/FGL.hs
+++ src/Jeep/Data/Graph/FGL.hs
@@ -1,11 +1,23 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Jeep.Data.Graph.FGL where
 
 -----------------------------------
 
-import System.Process ( readProcessWithExitCode )
-import System.Exit
+import Control.Arrow
+import Control.Exception
+import Control.Monad.RWS
+
+import Data.DList (singleton, fromList, toList)
+import Data.Graph.Inductive
+import Data.Graph.Inductive.Dot
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Tree
 
 import Data.Graph.Inductive.Graph
+
+import System.Process ( readProcessWithExitCode )
+import System.Exit
 
 -----------------------------------
 
@@ -27,5 +39,36 @@ invokeGraphvizDot :: FilePath -> String -> IO ExitCode
 invokeGraphvizDot dotfilePath outputType = do
   (exitCode, stdOut, stdErr) <- callCmd "dot" [dotfilePath,"-T" ++ outputType,"-O" ]
   return exitCode
+
+-----------------------------------
+
+-- | https://stackoverflow.com/questions/13355968/elegant-way-to-convert-a-tree-to-a-functional-graph-library-tree
+
+treeToGraph :: Tree a -> Gr a ()
+treeToGraph t = uncurry mkGraph . (toList *** toList) . snd $ evalRWS (go t) () [0..] where
+  go (Node a ns) = do
+    i <- state $ head &&& tail
+    es <- forM ns $ go >=> \j -> return (i, j, ())
+    tell (singleton (i, a), fromList es)
+    return i
+
+relabelGraph :: Gr a b -> (Node -> Node) -> Gr a b
+relabelGraph g f = mkGraph lnodes' ledges' where
+  lnodes' = (\(n,a) -> (f n,a)) <$> labNodes g
+  ledges' = (\(n1,n2,b) -> (f n1,f n2,b)) <$> labEdges g
+
+maxNode :: Gr a b -> Maybe Node
+maxNode g = if isEmpty g then Nothing else Just (snd (nodeRange g))
+
+insGraph :: Gr a b -> Gr a b -> Gr a b
+insGraph g1 g2 = -- trace (show (graphNodeRange g1) ++ " " ++ show (graphNodeRange g2) ) 
+  mkGraph (labNodes g1 ++ labNodes g2') (labEdges g1 ++ labEdges g2') where
+  nodeInc = fromMaybe 0 ((+ 1)  <$> maxNode g1)
+  g2' = relabelGraph g2 (+ nodeInc)
+  g1LNodes = labNodes g1
+
+forestToGraph :: [Tree a] -> Gr a ()
+forestToGraph ts = foldr insGraph empty gs where
+  gs = treeToGraph <$> ts
 
 -- End ---------------------------------------------------------------
