5d85c34d61907780c6ef49edc1e5ab22f2230e3b
Jerry Swan <dr.jerry.swan@gmail.com>
2025-05-25 09:04:14 +0100

    Add invoke Graphviz

--- src/Jeep/Data/Graph/FGL.hs
+++ src/Jeep/Data/Graph/FGL.hs
@@ -1,6 +1,10 @@
 module Jeep.Data.Graph.FGL where
 
 -----------------------------------
+
+import System.Process ( readProcessWithExitCode )
+import System.Exit
+
 import Data.Graph.Inductive.Graph
 
 -----------------------------------
@@ -12,5 +16,16 @@ implicitGraphToGraph nNodes nLabel eLabel = mkGraph mkLNodes mkLEdges where
 
 matrixToGraph :: Graph gr => [[a]] -> gr Int a
 matrixToGraph xss = implicitGraphToGraph (length xss) id (\i j -> (xss !! i) !! j )
+
+-----------------------------------
+
+callCmd :: String -> [String] -> IO (ExitCode, String, String)
+callCmd cmd args = readProcessWithExitCode cmd args stdIn where
+  stdIn = ""
+
+invokeGraphvizDot :: FilePath -> String -> IO ExitCode
+invokeGraphvizDot dotfilePath outputType = do
+  (exitCode, stdOut, stdErr) <- callCmd "dot" [dotfilePath,"-T" ++ outputType,"-O" ]
+  return exitCode
 
 -- End ---------------------------------------------------------------
