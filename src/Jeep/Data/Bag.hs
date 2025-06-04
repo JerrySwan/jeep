1981408c7a74d2dc1a4a76205099bcfb9f24d682
Jerry Swan <dr.jerry.swan@gmail.com>
2025-06-04 15:14:11 +0100

    Add sort to grouping in Bag ctors

--- src/Jeep/Data/Bag.hs
+++ src/Jeep/Data/Bag.hs
@@ -5,40 +5,49 @@ module Jeep.Data.Bag where
 import Data.List
 import Data.Maybe
 
-import qualified Data.Set as Set
-import qualified Data.Map as Map
+import qualified Data.Set as S
+import qualified Data.Map as M
 
 -----------------------------------
 
-newtype Bag a = Bag { asMap :: Map.Map a Int }
+newtype Bag a = Bag { asMap :: M.Map a Int } deriving (Eq)
 
 instance Show a => Show (Bag a) where
-  show (Bag m) = "{" ++ (intercalate "," str) ++ "}" where
-    str = f <$> Map.toList m 
+  show (Bag m) = "{" ++ intercalate "," str ++ "}" where
+    str = f <$> M.toList m
     f (k,c) = show k ++ "*" ++ show c
 
+-----------------------------------
+
+fromList :: (Eq a,Ord a) => [a] -> Bag a
+fromList xs = Bag $ M.fromList $ (\ys -> (head ys, length ys)) <$> group (sort xs)
+
+fromListBy :: Ord a => (a -> a -> Bool) -> [a] -> Bag a
+fromListBy f xs = Bag $ M.fromList $ (\ys -> (head ys, length ys)) <$> sort (groupBy f xs)
+
+
 -- https://www.reddit.com/r/haskell/comments/3an5xp/groupby_data_to_a_map/
-bagify :: Ord k => (v -> k) -> [v] -> Map.Map k [v]
-bagify key as = Map.fromListWith (++) as' where
+bagify :: Ord k => (v -> k) -> [v] -> M.Map k [v]
+bagify key as = M.fromListWith (++) as' where
   as' = map ((,) <$> key <*> (:[])) as
 
-fromList :: Ord a => [a] -> Bag a 
-fromList xs = Bag $ (\ys -> length ys) <$> bagify id xs
+-- fromList :: Ord a => [a] -> Bag a
+-- fromList xs = Bag $ (\ys -> length ys) <$> bagify id xs
 
-isBag :: Integral count => Map.Map a count -> Bool
-isBag m = all ((>=) 0) (Map.elems m)
+mapIsBag :: Integral count => M.Map a count -> Bool
+mapIsBag m = all (0 >=) (M.elems m)
 
-fromMap :: Map.Map a Int -> Maybe (Bag a)
-fromMap m = if isBag m then (Just $ Bag m ) else Nothing
+fromMap :: M.Map a Int -> Bag a
+fromMap m = Bag $ M.filter (0 >=) m 
 
 count :: Ord a => Bag a -> a -> Int
-count m x = fromMaybe 0 (Map.lookup x (asMap m)) 
+count m x = fromMaybe 0 (M.lookup x (asMap m))
 
-keySet :: Bag a -> Set.Set a
-keySet x = Map.keysSet (asMap x)
+keySet :: Bag a -> S.Set a
+keySet x = M.keysSet (asMap x)
 
 size :: Bag a -> Int
-size m = sum $ Map.elems (asMap m)
+size m = sum $ M.elems (asMap m)
 
 isEmpty :: Bag a -> Bool
 isEmpty m = size m == 0
